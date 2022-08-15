56bf0f0d06e580bdce6ad03fcf4965ee664d6945
Cesar <cesarvillalobosolmos.01@gmail.com>
2022-08-14 22:15:06 -0500

    Initial config

---
+++ README.md
@@ -0,0 +1,15 @@
+# Proyecto con codigo base para crear un proyecto con Vite
+
+Para poder utilizar esta plantilla base de configuracion con **Vite**, **JavaScript** y **Yarn** solo debemos usar los siguientes comandos: 
+
+Para instalar las dependencias:
+
+    yarn
+
+Para ejecutar el proyecto:
+
+    yarn dev
+
+Para crear el buil de produccion:
+
+    yarn build
--- main.js
+++ main.js
@@ -1,12 +1,13 @@
-import './style.css';
-import javascriptLogo from './javascript.svg';
-import { setupCounter } from './counter.js';
+import './src/css/style.css';
+import javascriptLogo from './assets/javascript.svg';
+import { setupCounter } from './src/js/counter.js';
 
 document.querySelector('#app').innerHTML = `
     <div>
         <a href="https://vitejs.dev" target="_blank">
-            <img src="/vite.svg" class="logo" alt="Vite logo" />
+            <img src="/assets/vite.svg" class="logo" alt="Vite logo" />
         </a>
+        
         <a href="https://developer.mozilla.org/en-US/docs/Web/JavaScript" target="_blank">
             <img src="${ javascriptLogo }" class="logo vanilla" alt="JavaScript logo" />
         </a>
